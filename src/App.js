4645df317c07addedeb5ea8863862b6ff250f10b
Ajay Kumar <ajay.kumar1@kellton.com>
2025-08-05 18:04:34 +0530

    Here i have earn the routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, {useState}  from 'react';
+import {BrowserRouter  as Router , Route,Redirect,Switch} from 'react-router-dom';
+
 import './App.css';
 import GoalList from './components/goal/goalList';
 import NewGoal from './components/newgoal/newgoal';
-
+import User from './users/pages/User';
+import NewPlaces from './places/pages/NewPlaces';
 
 const App= ()=>{
   const  [courseGoals,setCourseGoal] = useState([
@@ -13,15 +16,30 @@ const App= ()=>{
 
   const AddNewGoalHandler = (newGoal) => {
     setCourseGoal(courseGoals.concat(newGoal));
-  }
+   }
   return (
     
+  <Router>
+    <Switch>
+        <Route path = "/course" exact >
+          <div className='my-class'>
+            <h1>List of All Course Goals</h1>
+            <NewGoal onAddNewGoal = {AddNewGoalHandler} />
+            <GoalList goal = {courseGoals} />
+          </div>
+        </Route>
+        <Route path = "/" exact>
+              <User />
+        </Route>
+        <Route path = "/places/newplace" exact>
+          <NewPlaces />
+        </Route>
+        <Redirect to = "/" />
+    </Switch>
+ </Router>
   
-  <div className='my-class'>
-    <h1>List of All Course Goals</h1>
-    <NewGoal onAddNewGoal = {AddNewGoalHandler} />
-    <GoalList goal = {courseGoals} />
-  </div>
+  
+  
   
   );
 }
---
+++ src/places/pages/NewPlaces.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NewPlaces = () =>{
+
+    return (
+        <div>
+            <h1>Hi, I am a New Places</h1>
+        </div>
+    )
+}
+
+export default NewPlaces;
